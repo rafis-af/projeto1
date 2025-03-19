7256586f19e85172735d0bfd10507d5c25740565
rafis-af <rafa.ferreira0301@gmail.com>
2025-03-19 14:58:05 -0300

    criar tarefa

--- projeto..c
+++ projeto..c
@@ -3,7 +3,22 @@
 
 int criarTarefa(ListaDeTarefas *lt){
 
-    printf("Criar Tarefa\n");
+    if(lt -> qtd >= TOTAL_TAREFAS)
+    return 1;
+
+        Tarefa *t = &lt -> tarefas[lt -> qtd];
+
+    printf("Entre com a prioridade da tarefa: ");
+    scanf("%d", &t -> prioridade);
+
+    printf("Entre com a categoria da tarefa: ");
+    scanf("%s", t -> categoria);
+
+    printf("Entre com a descricao da tarefa: ");
+    scanf("%s", t -> descricao);
+
+    lt -> qtd++;
+
     return 0;
 }
 
@@ -32,6 +47,6 @@ int salvarTarefas(ListaDeTarefas *lt, char *nomeArquivo){
 
 void exibeMenu(){
     printf("Menu\n");
-    
+
 }
 
